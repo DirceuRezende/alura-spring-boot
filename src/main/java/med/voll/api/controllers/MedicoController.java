51f52a6cba82d80d31376d2782ebe77a6d88be72
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-04-28 16:48:33 -0300

    feat(Medico): add logical delete to a Medico.

--- src/main/java/med/voll/api/controllers/MedicoController.java
+++ src/main/java/med/voll/api/controllers/MedicoController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,19 +20,30 @@ public class MedicoController {
     private MedicoRepository repository;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
     @GetMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
     @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @Transactional
     public void atualizar(@RequestBody @Valid DadosAtualicacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Transactional
+    public void excluir(@PathVariable("id") Long id) {
+        var medico = repository.getReferenceById(id);
+        medico.excluir();
     }
 }
